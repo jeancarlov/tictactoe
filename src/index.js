cc3c3aaeb17ef18a330bb4aeff2c12eb6c803dda
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2020-02-01 21:41:31 -0500

    changing classes into function componentsbefore adding props

--- src/index.js
+++ src/index.js
@@ -2,45 +2,53 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-      return (
+// class Square extends React.Component {
+//     render() {
+//       return (
+//         <button className="square">
+//           {/* TODO */}
+//         </button>
+//       );
+//     }
+//   }
+
+  function Square() {
+    return (
         <button className="square">
           {/* TODO */}
         </button>
       );
-    }
   }
   
-  class Board extends React.Component {
-    renderSquare(i) {
+  function Board () {
+
+     function renderSquare(i) {
       return <Square />;
     }
-  
-    render() {
+    
       const status = 'Next player: X';
   
       return (
         <div>
           <div className="status">{status}</div>
           <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
           </div>
           <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
           </div>
           <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
           </div>
         </div>
       );
-    }
+
   }
   
   class Game extends React.Component {
